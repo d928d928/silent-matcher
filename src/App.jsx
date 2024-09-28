48fdb795047818ad9b92b2cc35432dd59eed59f3
root <root@UBUNTU>
2024-09-29 04:56:13 +0900

    feat: useStateの練習

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import { TestMessage } from "./components/TestMessage";
 
 export const App = () => {
+  const [num, setNum] = useState(0);
 
-  const onClickButton = () => alert();
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
 
   return (
     <>
       <h1 style={{ color: "red"}}>Props練習</h1>
       <TestMessage color="green">how are you?</TestMessage>
       <TestMessage color="blue">I'm fine.</TestMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップボタン</button>
+      <p>{num}</p>
     </>
   );
 };
